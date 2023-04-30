543a729667fb56f67e5266d4609b5fab706175d3
Oleh Hembarovskyi <gemboleg@gmail.com>
2023-04-30 16:56:55 +0300

    initial commit

---
+++ src/main/java/com/oh72/university/UniversityApplication.java
@@ -0,0 +1,17 @@
+package com.oh72.university;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	}
+}
